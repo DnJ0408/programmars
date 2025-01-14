fe99b0633c76cc0192ac0e8cf53550e8ae1a887c
YoungJin <oyj921205@gmail.com>
2025-01-14 10:20:20 +0900

    Add Create strange words

---
+++ Programmars/042. Create Strange words.js
@@ -0,0 +1,5 @@
+function solution(s) {
+    return s.split(' ').map(word => word.split('')
+            .map((char, index) => index % 2 === 0 ? char.toUpperCase() : char.toLowerCase())
+            .join('')).join(' ');
+}
